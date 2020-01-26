5d73285ea6edce43ce4be0edfd0a9632ac007c70
Bruna Iriz <bruna.iriz@opah.com.br>
2020-01-26 20:05:11 -0300

    add classe Person

---
+++ src/main/java/br/com/library/library/entity/Person.java
@@ -0,0 +1,29 @@
+package br.com.library.library.entity;
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotBlank
+    @Size(min = 11, max = 11)
+    private String cpf;
+
+    @NotBlank
+    private String name;
+
+    @NotNull
+    @Size(min = 10, max = 11)
+    private String telephone;
+}
