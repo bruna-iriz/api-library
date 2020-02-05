ee5b1dc332f46906a69043550fa6c1a334e9f99e
Bruna Iriz <bruna.iriz@opah.com.br>
2020-02-04 22:36:46 -0300

    criando entities

--- src/main/java/br/com/library/library/entity/SchoolSupplies.java
+++ src/main/java/br/com/library/library/entity/SchoolSupplies.java
@@ -1,16 +1,10 @@
 package br.com.library.library.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
 import java.io.Serializable;
 
 @Data
-@Entity
-@Table(name = "school_supplies")
 @NoArgsConstructor
 @AllArgsConstructor
 public class SchoolSupplies implements Serializable {
